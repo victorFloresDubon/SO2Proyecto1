9e31391712dd32ecfc8a9937c125e61cd4dc98e8
victorFloresDubon <vdubon@outlook.com>
2020-03-12 22:46:35 -0600

    Se redujo el No. de líneas de código para usarlas en otro archivo

--- src/ReemplazoDeMemoria.cpp
+++ src/ReemplazoDeMemoria.cpp
@@ -10,65 +10,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include "AlgoritmosReemplazo.h"
-#include "UtilidadesAlgoritmos.h"
+
 
 using namespace std;
-/* No. de marcos de página. */
-int numPaginas;
-/* No. de referencias. */
-int numReferencias;
-/* Arreglo que contendrá las referencias ingresadas */
-//int *paginas;
-/* Matriz que simular� la memoria dinmica, el es tamaño definido por
- * el usuario por medio de
- */
 
-/*
-void iniciarMatriz(int **matriz){
-	for(int i = 0; i < numPaginas; i++){
-		for(int j = 0; j < numReferencias; j++){
-			// Inicia los valores en la referencia 1
-			if (j == 0){
-				switch(i){
-				case 0:
-					matriz[i][j] = 1;
-					break;
-				case 1:
-					matriz[i][j] = 2;
-					break;
-				case 2:
-					matriz[i][j] = 3;
-					break;
-				case 3:
-					matriz[i][j] = 4;
-					break;
-				}
-			}else{
-				matriz[i][j] = -1;
-			}
-		}
-	}
-}
-
-void imprimirMatrizPrincipal(int **matriz){
-	for(int i = 0; i < numPaginas; i++){
-		printf("Pagina %d -> ",i+1);
-		for(int j = 0; j < numReferencias; j++){
-			printf("%d\t",matriz[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void insertaReferencia(){
-	for(int i = 0; i < numPaginas; i++){
-		printf("Ingrese la referencia No. %d -> ",i+1);
-		scanf("%d",&paginas[i]);
-	}
-}
-*/
 void menu(){
-//	int **matriz;
 
 	int opc;
 	do{
@@ -82,39 +28,6 @@ void menu(){
 		printf("4.\t\t Salir\n");
 		printf("-> ");
 		scanf("%d",&opc);
-		if (opc != 4){
-			printf("Ingrese el Numero de paginas que desea -> ");
-			scanf("%d", &numPaginas);
-			printf("\n");
-			printf("Ingrese el Numero de referencias que desea -> ");
-			scanf("%d", &numReferencias);
-			printf("\n");
-			switch(opc){
-			case 1:
-
-				break;
-			case 2:
-				break;
-			case 3:
-				lru(numPaginas,numReferencias);
-				break;
-			default:
-				printf("Opcion no existe.\n;");
-				system("cls");
-
-			}
-			// Llena la matriz
-/*			paginas = new int [numPaginas];
-			matriz = new int *[numPaginas];
-			   for (int i = 0;i < numPaginas;i++ ){
-				   matriz[i]= new int [numReferencias];
-			   }
-			iniciarMatriz(matriz);
-*/
-			/* Imprime la matriz */
-		}else{
-			printf("Gracias, vuelva pronto\n");
-		}
 	}while(opc!=4);
 	system("PAUSE");
 }
